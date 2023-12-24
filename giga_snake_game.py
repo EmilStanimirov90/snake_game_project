c6feafc4d09090f68af3da92510345dc3a3af28d
Emil Stanimirov <145929264+EmilStanimirov90@users.noreply.github.com>
2023-12-24 23:05:03 +0200

    update

--- giga_snake_game.py
+++ giga_snake_game.py
@@ -1,13 +1,14 @@
+from collections import deque
+
 import pygame
 from random import randint
 
 
-def generate_food_at_random_position(scr):
-    x = randint(0, 800)  # Replace 800 with your desired maximum x-coordinate
-    y = randint(0, 800)  # Replace 800 with your desired maximum y-coordinate
-    pink = (240, 60, 123)
-    food = pygame.draw.circle(scr, pink, (x, y), 10)
-    return food
+def generate_food_random_position(_x, _y):
+    x = randint(0, _x)  # Replace 800 with your desired maximum x-coordinate
+    y = randint(0, _y)  # Replace 800 with your desired maximum y-coordinate
+
+    return (x // cell_size) * cell_size, (y // cell_size) * cell_size
 
 
 pygame.init()
@@ -18,21 +19,31 @@ game_board_y = 800
 game_board = pygame.display.set_mode((game_board_x, game_board_y))
 pygame.display.set_caption("Giga-Mecha-Snake")
 
-clock = pygame.time.Clock()
-
-generate_food_at_random_position(game_board)
-
-#game variables
+# game variables
 cell_size = 20
-direction = 1 # 1 is up 2 is down 3 is right 4 is left
+direction = ""  # 1 is up 2 is down 3 is right 4 is left
+moving = False
+eaten = True
+food_x = 0
+food_y = 0
+total_score = 0
 
 # create snake
-snake_position = [[int(game_board_x / 2), int(game_board_y / 2)],
-                  [int(game_board_x / 2), int(game_board_y / 2) + cell_size],
-                  [int(game_board_x / 2), int(game_board_y / 2) + cell_size * 2],
-                  [int(game_board_x / 2), int(game_board_y / 2) + cell_size * 3],
-                  [int(game_board_x / 2), int(game_board_y / 2) + cell_size * 4]
-                  ]
+snake_position = deque([[int(game_board_x / 2), int(game_board_y / 2)],
+                        [int(game_board_x / 2), int(game_board_y / 2) + cell_size],
+                        [int(game_board_x / 2), int(game_board_y / 2) + cell_size * 2],
+                        [int(game_board_x / 2), int(game_board_y / 2) + cell_size * 3],
+                        [int(game_board_x / 2), int(game_board_y / 2) + cell_size * 4]
+                        ])
+snake_head = snake_position[0]
+
+# possible moves
+moves = {"up": (0, -cell_size), "down": (0, cell_size), "right": (cell_size, 0), "left": (-cell_size, 0)}
+
+# game speed
+clock = pygame.time.Clock()
+snake_speed = 2
+
 # player_image = pygame.image.load('head.png')  # Load the player image
 # snake_head_rect = player_image.get_rect()  # Get the rectangle that encloses the image
 # snake_head_rect.center = (snake_position[0])
@@ -46,7 +57,7 @@ light_python_grey = (60, 63, 65)
 dark_python_grey = (43, 43, 43)
 pink = (240, 60, 123)
 
-# setup game loop with exit
+# setup game LOOP with exit
 running = True
 while running:
 
@@ -56,43 +67,56 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
         elif event.type == pygame.KEYDOWN:
-            if (event.key == pygame.K_UP or event.key == pygame.K_w) and direction != 2:
-                direction = 1
-            if (event.key == pygame.K_DOWN or event.key == pygame.K_s) and direction != 1:
-                direction = 2
-            if (event.key == pygame.K_RIGHT or event.key == pygame.K_d) and direction != 4:
-                direction = 3
-            if (event.key == pygame.K_LEFT or event.key == pygame.K_a) and direction != 3:
-                direction = 4
+            if (event.key == pygame.K_UP or event.key == pygame.K_w) and direction != "down":
+                direction = "up"
+                moving = True
+            if (event.key == pygame.K_DOWN or event.key == pygame.K_s) and direction != "up":
+                direction = "down"
+                moving = True
+            if (event.key == pygame.K_RIGHT or event.key == pygame.K_d) and direction != "left":
+                direction = "right"
+                moving = True
+            if (event.key == pygame.K_LEFT or event.key == pygame.K_a) and direction != "right":
+                direction = "left"
+                moving = True
+    # move
+    if moving:
+        snake_position.appendleft([snake_position[0][0] + moves[direction][0], snake_position[0][1] + moves[direction][1]])
+        snake_position.pop()
 
-    snake_position = snake_position[-1:] + snake_position[:-1]
+    # generate food and place food
+    if eaten:
+        food_x, food_y = generate_food_random_position(game_board_x, game_board_y)
+        eaten = False
 
+    # place food on game_board
+    food = pygame.draw.rect(game_board, pink, (food_x, food_y, cell_size, cell_size))
 
-
-
-    # draw snake
+    # draw snake + moving
     for i in range(len(snake_position)):
         if i % 2 == 0:
-            pygame.draw.circle(game_board, green, (snake_position[i][0], snake_position[i][1]), cell_size / 2)
+            pygame.draw.rect(game_board, green, (snake_position[i][0], snake_position[i][1], cell_size, cell_size))
         else:
-            pygame.draw.circle(game_board, orange, (snake_position[i][0], snake_position[i][1]), cell_size / 2)
+            pygame.draw.rect(game_board, orange, (snake_position[i][0], snake_position[i][1], cell_size, cell_size))
 
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_LEFT]:
-    #     snake_head_rect.x -= 20
-    # if keys[pygame.K_RIGHT]:
-    #     snake_head_rect.x += 20
-    # if keys[pygame.K_UP]:
-    #     snake_head_rect.y -= 20
-    # if keys[pygame.K_DOWN]:
-    #     snake_head_rect.y += 20
+    # eating food and growing
+    if snake_position[0] == [food_x, food_y]:
+        eaten = True
+        snake_position.append([food_x, food_y])
+        total_score += 1
+
+    #display_score
+    score_text = font.render(f"Score: {total_score}", True, )
+    game_board.blit(score_text, (10, 10))
+
 
     # game_board.blit(player_image, snake_head_rect)  # Draw the player image at its current position
-    pygame.display.flip()
-    # pygame.display.update()
-    clock.tick(10)
+    # pygame.display.flip()
+    pygame.display.update()
 
-pygame.display.flip()
+    clock.tick(snake_speed)
+# pygame.display.flip()
 
 pygame.quit()
